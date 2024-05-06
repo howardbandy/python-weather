5befb7a565dee678f201f2b103c6fdf39b962ecf
howardbandy <howardbandy@gmail.com>
2024-05-06 15:45:39 -0700

    update python files

--- weather.py
+++ weather.py
@@ -5,10 +5,14 @@ import os
 
 load_dotenv()
 
-def get_current_weather(city='Chicago'):
-  request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
-  weather_data = requests.get(request_url).json()
-  return weather_data
+def get_current_weather(city="Kansas City"):
+
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
 
 if __name__ == '__main__':
   print('\n*** Get Current Weather Conditions ***\n')
